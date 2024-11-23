3e9313f8626096fccd977ebf07d06c190b728bf5
crypto0627 <jake0627a1@gmail.com>
2024-11-23 22:45:09 +0800

    feat: image domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+  images: {
+    domains: ['www.jakekuo.com'],
+},
 }
 
 export default nextConfig
